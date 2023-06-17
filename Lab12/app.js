4e383bb4c081b1607a804ce1faca73642883e72c
dheynoshan <dheynoshan@live.com>
2023-06-16 22:59:39 -0500

    sub

--- Lab12/app.js
+++ Lab12/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.enable("case sensitive routing");
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname)));
 
 
 app.use("/products",productRouter);
@@ -15,6 +15,10 @@ app.use("/user",userRouter);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.use(function(err, req, res, next) {
+  res.status(500).send('Internal Error');
 });
 
 app.listen(3000, () => console.log("Listening on 3000"));
